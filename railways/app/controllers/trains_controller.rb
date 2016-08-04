0d3831aa4fcedfd132acd8d1147e82b31bdc7775
akudashkin <akudashkin@yandex.ru>
2016-08-04 15:32:15 -0400

    Cleaning the scaffold

--- railways/app/controllers/trains_controller.rb
+++ railways/app/controllers/trains_controller.rb
@@ -1,54 +1,27 @@
 class TrainsController < ApplicationController
   before_action :set_train, only: [:show, :edit, :update, :destroy]
 
-  # GET /trains
-  # GET /trains.json
   def index
     @trains = Train.all
   end
 
-  # GET /trains/1
-  # GET /trains/1.json
   def show
   end
 
-  # GET /trains/new
   def new
     @train = Train.new
   end
 
-  # GET /trains/1/edit
   def edit
   end
 
-  # POST /trains
-  # POST /trains.json
   def create
     @train = Train.new(train_params)
-
-    respond_to do |format|
-      if @train.save
-        format.html { redirect_to @train, notice: 'Train was successfully created.' }
-        format.json { render :show, status: :created, location: @train }
-      else
-        format.html { render :new }
-        format.json { render json: @train.errors, status: :unprocessable_entity }
-      end
-    end
+    @train.save ? redirect_to(@train, notice: 'Train was successfully created.') : render(:new)
   end
 
-  # PATCH/PUT /trains/1
-  # PATCH/PUT /trains/1.json
   def update
-    respond_to do |format|
-      if @train.update(train_params)
-        format.html { redirect_to @train, notice: 'Train was successfully updated.' }
-        format.json { render :show, status: :ok, location: @train }
-      else
-        format.html { render :edit }
-        format.json { render json: @train.errors, status: :unprocessable_entity }
-      end
-    end
+    @train.update(train_params) ? redirect_to(@train, notice: 'Train was successfully updated.') : render(:edit)
   end
 
   def destroy
