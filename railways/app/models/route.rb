390d278789a35b2aaec264574e1b4868e3b51663
akudashkin <akudashkin@yandex.ru>
2016-08-07 12:34:15 -0400

    Set name of route by callback

--- railways/app/models/route.rb
+++ railways/app/models/route.rb
@@ -2,4 +2,13 @@ class Route < ActiveRecord::Base
   validates :name, presence: true
   has_and_belongs_to_many :railway_stations
   has_many :trains
+
+  before_validation :set_name
+
+  private
+    def set_name
+      if self.name.blank?
+        self.name = "#{railway_stations.first.title} - #{railway_stations.last.title}" 
+      end
+    end
 end
