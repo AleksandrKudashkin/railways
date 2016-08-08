2f39a1c45b8c3761189ac076a6de92c789ca40e5
akudashkin <akudashkin@yandex.ru>
2016-08-07 23:12:26 -0400

    Show sorted stations at the Route

--- railways/app/models/railway_station.rb
+++ railways/app/models/railway_station.rb
@@ -3,6 +3,10 @@ class RailwayStation < ActiveRecord::Base
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
   has_many :trains
+  
+  scope :sorted, ->(route) { 
+    includes(:railway_stations_routes).where('railway_stations_routes.route_id = ?', route.id).order('railway_stations_routes.number') 
+  }
 
   def get_position(route)
     self.railway_stations_routes.find_by_route_id(route).number
