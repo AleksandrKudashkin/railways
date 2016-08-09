8abcbf70c3b3b758de387bbccc2e171462c14e1c
Aleksandr Kudashkin <alexkudashkin@gmail.com>
2016-08-09 13:35:21 -0400

    Precise validations for Coach

--- railways/app/models/coach.rb
+++ railways/app/models/coach.rb
@@ -11,20 +11,20 @@ end
 
 class Suburban < Coach
   validates :simple_seats, presence: true, numericality: { only_integer: true }
+  validates :top_seats, :bottom_seats, :side_top_seats, :side_bottom_seats, absence: true
 end
 
 class Economy < Coach
-  validates :top_seats, presence: true, numericality: { only_integer: true }
-  validates :bottom_seats, presence: true, numericality: { only_integer: true }
-  validates :side_top_seats, presence: true, numericality: { only_integer: true }
-  validates :side_bottom_seats, presence: true, numericality: { only_integer: true }
+  validates :top_seats, :bottom_seats, :side_top_seats, :side_bottom_seats, presence: true, numericality: { only_integer: true }
+  validates :simple_seats, absence: true
 end
 
 class Compartment < Coach
-  validates :top_seats, presence: true, numericality: { only_integer: true }
-  validates :bottom_seats, presence: true, numericality: { only_integer: true }
+  validates :top_seats, :bottom_seats, presence: true, numericality: { only_integer: true }
+  validates :simple_seats, :side_top_seats, :side_bottom_seats, absence: true
 end
 
 class Sleeping < Coach
   validates :bottom_seats, presence: true, numericality: { only_integer: true }
+  validates :top_seats, :simple_seats, :side_top_seats, :side_bottom_seats, absence: true
 end
